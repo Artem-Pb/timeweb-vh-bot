bab3953899376448b38ccd4f069bb5f9d8ef724b
Artem-Pb <kenni506@icloud.com>
2026-09-21 02:40:44 +0700

    feat: inline-клавиатуры меню бота

---
+++ keybords.py
@@ -0,0 +1,54 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+def get_start_keyboard() -> InlineKeyboardMarkup:
+    inl = InlineKeyboardBuilder()
+    inl.add(
+        InlineKeyboardButton(
+            text="Домены",
+            callback_data="domain"),
+        InlineKeyboardButton(
+            text="Сайты",
+            callback_data="sites"),
+        InlineKeyboardButton(
+            text="Баланс",
+            callback_data="balance"),
+    )
+
+    inl.adjust(1, 1, 1)
+    return inl.as_markup()
+
+def get_domains() -> InlineKeyboardMarkup:
+    inl = InlineKeyboardBuilder()
+    inl.add(
+        InlineKeyboardButton(
+            text="Домой/назад",
+            callback_data="back"
+        )
+    )
+    inl.adjust(1)
+    return inl.as_markup()
+
+def get_sites() -> InlineKeyboardMarkup:
+    inl = InlineKeyboardBuilder()
+    inl.add(
+        InlineKeyboardButton(
+            text="Домой/назад",
+            callback_data="back"
+        )
+    )
+    inl.adjust(1)
+    return inl.as_markup()
+
+def get_balance() -> InlineKeyboardMarkup:
+    inl = InlineKeyboardBuilder()
+    inl.add(
+        InlineKeyboardButton(
+            text="Домой/назад",
+            callback_data="back"
+        )
+    )
+    inl.adjust(1)
+    return inl.as_markup()
+
+
